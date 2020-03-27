e77cd695efdb9785fcd329a14c1e57f07ade2efa
taejin-kim <drumtj@gmail.com>
2020-03-27 17:01:15 +0900

    'deploy'

--- control.js
+++ control.js
@@ -2,6 +2,107 @@ const {PythonShell} = require("python-shell");
 const clipboardy = require('clipboardy');
 const robot = require("robotjs");
 
+const processWindows = require("node-process-windows");
+//executeProcess에 "chcp 65001 | " 추가해야 한글 안깨짐
+
+const { DModel, DStruct, K,U } = require('win32-api');
+const ref = require('ref-napi');
+const StructDi = require('ref-struct-di');
+const Struct = StructDi(ref);
+const knl32 = K.load();
+const user32 = U.load();  // load all apis defined in lib/{dll}/api from user32.dll
+// const user32 = U.load(['FindWindowExW'])  // load only one api defined in lib/{dll}/api from user32.dll
+
+function getPid(title){
+  return new Promise(resolve=>{
+    PythonShell.run('win.py', {
+      mode: 'json',
+      // pythonOptions: ['-u'],
+      args: ["getPid", title]
+    }, function (err, results) {
+      if (err) throw err;
+      resolve(results[0]);
+    });
+  })
+}
+module.exports.getPid = getPid;
+
+function focusWindow(pid){
+  return new Promise(resolve=>{
+    PythonShell.run('win.py', {
+      mode: 'json',
+      // pythonOptions: ['-u'],
+      args: ["focus", pid]
+    }, function (err, results) {
+      if (err) throw err;
+      // results is an array consisting of messages collected during execution
+      // console.log('results: %j', results);
+      resolve(results[0]);
+    });
+  })
+}
+module.exports.focusWindow = focusWindow;
+
+function getProcessList(){
+  return new Promise(resolve=>{
+    processWindows.getProcesses(function(err, processes) {
+      // let list = [];
+      // processes.forEach(function (p) {
+      //   if(p.mainWindowTitle){
+      //     list.push(p);
+      //     console.log("PID: " + p.pid.toString());
+      //     // console.log("MainWindowTitle: " + p.mainWindowTitle);
+      //     console.log("ProcessName: " + p.processName);
+      //   }
+      // });
+      // resolve(list);
+      resolve(processes.filter(p=>!!p.mainWindowTitle))
+    });
+  })
+}
+module.exports.getProcessList = getProcessList;
+
+function getWindowHandle(title){
+  title = title + '\0';    // null-terminated string
+
+  const lpszWindow = Buffer.from(title, 'ucs2');
+  const hWnd = user32.FindWindowExW(0, 0, null, lpszWindow);
+
+  if (typeof hWnd === 'number' && hWnd > 0
+    || typeof hWnd === 'bigint' && hWnd > 0
+    || typeof hWnd === 'string' && hWnd.length > 0
+  ) {
+    return hWnd;
+  }
+  return null;
+}
+module.exports.getWindowHandle = getWindowHandle;
+
+function setWindowTitle(hWnd, title){
+  if(hWnd == null) return false;
+
+  const res = user32.SetWindowTextW(hWnd, Buffer.from(title+'\0', 'ucs2'))
+  if (!res) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+module.exports.setWindowTitle = setWindowTitle;
+
+function getWindowRect(hWnd){
+  if (hWnd !== null) {
+    let rect = new Struct(DStruct.RECT)();
+    user32.GetWindowRect(hWnd, rect.ref());
+    return {
+      left:rect.left, top:rect.top, right:rect.right, bottom:rect.bottom,
+      width:rect.right-rect.left, height:rect.bottom-rect.top
+    }
+  }
+  return null;
+}
+module.exports.getWindowRect = getWindowRect;
 
 
 function move(x, y){
--- test2.js
+++ test2.js
@@ -1,101 +1,64 @@
-const robot = require("robotjs");
+
 //const wincmd = require("node-windows");
 // const { snapshot } = require("process-list");
-const processWindows = require("node-process-windows");
 
-const {PythonShell} = require("python-shell");
 // let pyshell = new PythonShell('test.py');
 
 var fsp = require("promise-fs");
 var exec = require("child-process-promise").exec;
 // PythonShell.defaultOptions = { scriptPath: './' };
+const tesseract = require("node-tesseract-ocr");
 
 const control = require("./control.js");
 
-// sends a message to the Python script via stdin
-
-// const Iconv = require('iconv').Iconv;
-// const iconv  = require('iconv-lite');
-// const { Transform } = require("stream");
-// const WindowInfo = require("window-info").default;
-const { DModel, DStruct, K,U } = require('win32-api');
-const ref = require('ref-napi');
-const StructDi = require('ref-struct-di');
-const Struct = StructDi(ref);
-
-const knl32 = K.load();
-const user32 = U.load();  // load all apis defined in lib/{dll}/api from user32.dll
-// const user32 = U.load(['FindWindowExW'])  // load only one api defined in lib/{dll}/api from user32.dll
-
-const title = '계산기\0';    // null-terminated string
-
-const lpszWindow = Buffer.from(title, 'ucs2');
-const hWnd = user32.FindWindowExW(0, 0, null, lpszWindow);
-
-if (typeof hWnd === 'number' && hWnd > 0
-  || typeof hWnd === 'bigint' && hWnd > 0
-  || typeof hWnd === 'string' && hWnd.length > 0
-) {
-  console.log('buf: ', hWnd);
-
-  // Change title of the Calculator
-  // const res = user32.SetWindowTextW(hWnd, Buffer.from('Node-Calculator\0', 'ucs2'))
-
-  // if ( ! res) {
-  //   console.log('SetWindowTextW failed')
-  // }
-  // else {
-  //   console.log('window title changed')
-  // }
-
-  for(var o in user32){
-    console.error(o);
-  }
-  let rect = new Struct(DStruct.RECT)();
-  user32.GetWindowRect(hWnd, rect.ref());
-  var screenSize = robot.getScreenSize();
-  console.error(rect.left, rect.top, rect.right, rect.bottom, screenSize);
-
-  // user32.BringWindowToTop(hWnd);
-  // user32.ShowWindow(hWnd, 5);
-  // user32.UpdateWindow(hWnd);
-
-  // robot.moveMouse(rect.left+100, rect.top+20);
-  // robot.mouseToggle("down");
-  // robot.dragMouse(789, 500);
-  // robot.mouseToggle("up");
-
-
-  // processWindows.getProcesses(function(err, processes) {
-  //   processes.forEach(function (p) {
-  //     if(p.mainWindowTitle){
-  //       console.log("PID: " + p.pid.toString());
-  //       console.log("MainWindowTitle: " + p.mainWindowTitle);
-  //       console.log("ProcessName: " + p.processName);
-  //     }
-  //   });
-  // });
-}
 
 test();
 
 //http://blog.naver.com/PostView.nhn?blogId=htblog&logNo=221510551432&parentCategoryNo=&categoryNo=99&viewDate=&isShowPopularPosts=true&from=search
 async function test(){
-  let clickSuccess;// = await control.imageClick("test.png");
-  while(!clickSuccess){
-    console.error("find image");
-    clickSuccess = await control.imageClick("test.png");
-    if(clickSuccess){
-      await control.typewrite("rlaxowls");
-    }
-  }
-  
+  // let clickSuccess;// = await control.imageClick("test.png");
+  // while(!clickSuccess){
+  //   console.error("find image");
+  //   clickSuccess = await control.imageClick("test.png");
+  //   if(clickSuccess){
+  //     await control.typewrite("rlaxowls");
+  //   }
+  // }
+
   // await control.key("press", "a");
   // await control.key("keyDown", "b");
-  await control.copy("sfsfsefsfd");
+  // await control.copy("sfsfsefsfd");
   // await control.key("hotkey", "ctrl", "v");
-  await control.paste();
+  // await control.paste();
+  //
+  
+  //이부분 뭔가 오작동함.. 다른 언어를 써야할까 ?
+  let processList = await control.getProcessList();
 
+  // console.error({processList});
+  await control.focusWindow(36948);
+  // console.error(await control.getPid("계산기"));
+  // return;
+
+  let hWnd = control.getWindowHandle("계산기");
+
+  let rect = control.getWindowRect(hWnd);
+  console.error(rect);
   // await control.screenshot("sc4.png");
-  // await control.screenshot("sc5.png", 0, 0, 300, 300);
+  if(0&&rect){
+    await control.screenshot("sc6.png", rect.left, rect.top, rect.width, rect.height);
+    tesseract.recognize("sc6.png", {
+      lang: "kor",
+      oem: 1,
+      psm: 3,
+      // tessedit_char_whitelist: "0123456789",
+    })
+    .then(text => {
+      console.log("Result:", text)
+    })
+    .catch(error => {
+      console.log(error.message)
+    })
+  }
+
 }
