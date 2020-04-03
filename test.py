99c84b857aa3c02b74a3ab641df9effd27d50ed2
taejin-kim <drumtj@gmail.com>
2020-04-03 17:03:38 +0900

    'deploy'

--- test.py
+++ test.py
@@ -1,45 +1,18 @@
-import win32gui
-import win32process
-import win32con, win32api
-import psutil
-# def winEnumHandler( hwnd, ctx ):
-#     if win32gui.IsWindowVisible( hwnd ):
-#         print (hex(hwnd), win32gui.GetWindowText( hwnd ))
-#
-# win32gui.EnumWindows( winEnumHandler, None )
+from selenium import webdriver
 
-def GetWindowList():
-    """
-    returns window title list
-    based on this answer - https://stackoverflow.com/a/31280850
-    """
+profile = webdriver.FirefoxProfile()
+profile.set_preference("network.proxy.type", 1)
+profile.set_preference("network.proxy.socks", "192.161.10.101")
+# profile.set_preference("network.proxy.socks", "127.0.0.1")
+profile.set_preference("network.proxy.socks_port", 80)
+profile.update_preferences()
+driver = webdriver.Firefox(profile)
 
-    titles = []
-    t = []
-    pidList = [(p.pid, p.name()) for p in psutil.process_iter()]
+# PROXY = "http://127.0.0.1:9050"
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--proxy-server=%s' % PROXY)
+# driver = webdriver.Chrome(chrome_options=chrome_options)
 
-    def enumWindowsProc(hwnd, lParam):
-        """ append window titles which match a pid """
-        if (lParam is None) or ((lParam is not None) and (win32process.GetWindowThreadProcessId(hwnd)[1] == lParam)):
-            text = win32gui.GetWindowText(hwnd)
-            if text:
-                wStyle = win32api.GetWindowLong(hwnd, win32con.GWL_STYLE)
-                if wStyle & win32con.WS_VISIBLE:
-                    t.append("%s" % (text))
-                    return
-
-    def enumProcWnds(pid=None):
-        win32gui.EnumWindows(enumWindowsProc, pid)
-
-    for pid, pName in pidList:
-        enumProcWnds(pid)
-        if t:
-            for title in t:
-                titles.append({'pname':pName, 'title':title, 'pid':pid})
-                # titles.append("('{0}', '{1}')".format(pName, title))
-            t = []
-
-    titles = sorted(titles, key=lambda x: x['title'].lower())
-    return titles
-
-print(GetWindowList())
+driver.get('http://icanhazip.com/')
+print(driver.page_source)
+# driver.quit()
